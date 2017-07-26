3c5cdb0b29ed80e3b153658f768f05983806a5a6
maknoon <conniemob@gmail.com>
2017-07-26 15:07:35 -0700

    fixed small pointer issue & wrote tests for rpmb

--- week1/rpmb.cc
+++ week1/rpmb.cc
@@ -17,10 +17,9 @@
 //== DECLARATIONS
 int NumChars(char* begin, char* end);
 
-
 //== DEFINITIONS
-#define MASK    0x80
-#define SHIFT   7
+#define MASK    0x80  // 0x80 = b10000000
+#define SHIFT   7     // shift 7 bits to the highest bit in a byte
 #define JAPN    2
 #define ENGL    1
 #define MALF    0
@@ -28,25 +27,29 @@ int NumChars(char* begin, char* end);
 //== FUNCTIONS
 int NumChars(char* begin, char* end)
 {
-  // we always need to check the previous char as well
-  // so find the previous by iterating to it
-  char* c = end;
-  char* prev = begin;
-  while (*prev != end) prev++;
+  // the last byte in the string should be where end points to
+  // find the previous byte by iterating to it from begin
+  char c = end[0];
+  char prev = begin[0];
+  for (int i = 0; prev != *end; i++) prev = begin[i];
 
   // CASE 1: HIGH BIT IS HIGH
+  // a) 1,1 = Japanese
+  // b) 0,1 = malformed
   if ((c & MASK) >> SHIFT) {
     if ((prev & MASK) >> SHIFT) return JAPN;
     else return MALF;
   }
 
   // CASE 2: HIGH BIT IS LOW
+  // a) 1,0 = Japanese
+  // b) 0,0 = English
   else {
     if ((prev & MASK) >> SHIFT) return JAPN;
-    else return ENGLISH;
+    else return ENGL;
   }
 
-  // should never reach this point
+  // should never reach this point, but just in case
   return MALF;
 }
 
@@ -54,20 +57,36 @@ int NumChars(char* begin, char* end)
  * for testing purposes
  */
 void tests(void) {
-  // test case 1: malformed
+  // TODO implement cassert library with this
+  int result = 0;
+  // test case 1: malformed -- not sure how to test
+
   // test case 2: "english"
+  std::string c2 = "english";
+  std::cout << "testing: english" << std::endl;
+  result = NumChars(&(c2[0]), &(c2[c2.size()-1]));
+  std::cout << "Last character is: " << result << std::endl;
+
   // test case 3: "にほんご"
+  std::string c3 = "にほんご";
+  std::cout << "testing: にほんご" << std::endl;
+  result = NumChars(&(c3[0]), &(c3[c3.size()-1]));
+  std::cout << "Last character is: " << result << std::endl;
 }
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
-  char* input; int type = 0;
-  std::cout << "Enter a string to test: ";
-  std::cin >> input;
+  if (argc > 1) tests();
 
-  type = NumChars(input[0], input[sizeof(input)-1]);
-  std::cout << "Last character of string is: " << type << std::endl;
+  else {
+    std::string input; int type = 0;
+    std::cout << "Enter a string to test: ";
+    std::cin >> input;
+    //std::cout << "size of input: " << input.size() << std::endl;
+    type = NumChars(&(input[0]), &(input[input.size()-1]));
+    std::cout << "Last character of string is: " << type << std::endl;
+  }
 
   return 0;
 }
